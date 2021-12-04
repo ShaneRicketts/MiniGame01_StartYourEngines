8b1136280d9289e1cc769efec8d0499562fff106
LeveledRogue <shanerickettswork@gmail.com>
2021-12-04 15:39:30 -0500

    updated sfx

--- Assets/Scripts/ImpactReaction.cs
+++ Assets/Scripts/ImpactReaction.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class ImpactReaction : MonoBehaviour
 {
+    public bool playsOnPlayerHit;
     public AudioClip impactSound;
+    private bool canMakeSound;
+    private float waitTilStart = 1f;
+    public float soundResetTime = 2;
 
     void Start()
     {
         GetComponent<AudioSource>().playOnAwake = false;
         GetComponent<AudioSource>().clip = impactSound;
+        InvokeRepeating("AllowSound", waitTilStart, soundResetTime);
     }
 
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision other)
+    {
+        if (canMakeSound && impactSound != null)
+        {
+            if (playsOnPlayerHit)
+            {
+                if (other.gameObject.CompareTag("Player"))
+                {
+                    PlaySound();
+                }
+            }
+            else if (!playsOnPlayerHit)
+            {
+                PlaySound();
+            }
+        }
+    }
+
+    void AllowSound()
+    {
+        canMakeSound = true;
+    }
+    void PlaySound()
     {
         GetComponent<AudioSource>().Play();
+        canMakeSound = false;
     }
 }
